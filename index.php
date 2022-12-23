da906abc68a0d9eeca1ea9dae0cee799732ca83c
luciendupont <lucienluludupont@gmail.com>
2022-12-23 10:49:06 +0100

    php

---
+++ index.php
@@ -0,0 +1,10 @@
+
+ <?php 
+    $couleur[] = "red";
+    $couleur[] = "blue";
+    $couleur[] = "white";
+    $couleur[] = "black";
+
+    // pour afficher la valeur "white", on écrira :
+    echo $couleur[2];
+?>
