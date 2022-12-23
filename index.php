fd7bc37856fb1209deddc1797620e98a4b5d80da
luciendupont <lucienluludupont@gmail.com>
2022-12-23 10:49:18 +0100

    php

--- index.php
+++ index.php
@@ -1,10 +1,25 @@
 
  <?php 
-    $couleur[] = "red";
-    $couleur[] = "blue";
-    $couleur[] = "white";
-    $couleur[] = "black";
 
-    // pour afficher la valeur "white", on écrira :
-    echo $couleur[2];
+$min = 0;
+$max = 100;
+ 
+$chiffresPairs = '25';
+ 
+for ($i = $min; $i <= $max; $i++) {
+    if ( $i % 2 == 0 ) {
+        $chiffresPairs .= $i.' ';
+    }
+}
+ 
+echo "Les chiffres pairs compris entre $min et $max sont: $chiffresPairs";
+
+
+
 ?>
+
+
+
+
+
+
