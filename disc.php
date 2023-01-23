8e2b6ce55b3ebde3f453897bc6be17bcc3a47b9d
luciendupont <lucienluludupont@gmail.com>
2023-01-23 09:20:51 +0100

    notif

--- disc.php
+++ disc.php
@@ -17,7 +17,7 @@ $requete->closeCursor();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="/assets/css/style.css">
-    <title>disque</title>
+    <title>Velvet Record_disque</title>
 </head>
 <body>
     <div class="container-fluid">      
@@ -30,8 +30,8 @@ $requete->closeCursor();
             <tr>
             <td><img src="jacquette/<?= $disc->disc_picture?>" width= 200 alt="" class="img-thumbnail" alt="Cinque Terre"></td>
                 <td class="fs-6">
-                <span class="fs-1"><?=$disc->disc_title?></span><br>
-                <span class="fs-2"><?=$disc->artist_name?></span><br>              
+                <span class="fs-1">titre : <?=$disc->disc_title?></span><br>
+                <span class="fs-2">artiste :<?=$disc->artist_name?></span><br>              
                 <span class="fw-bold">Label : </span> <?=$disc->disc_label?><br>              
                 <span class="fw-bold">année : </span> <?=$disc->disc_year?><br>               
                 <span class="fw-bold">Genre :</span> <?=$disc->disc_genre?><br>
